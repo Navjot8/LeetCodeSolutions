d81523e4e65607012f9d230e047c9562bbddf78e
Navjot <navjotsinghsarwara95@gmail.com>
2021-11-02 00:06:39 -0400

    Technique: sorting he dictionary by values

--- Majority Element.py
+++ Majority Element.py
@@ -1,9 +1,14 @@
-nums = [3,2,3]
-newNums=[]
-for i in nums:
-    if i not in newNums:
-        newNums.append(i)
-dic={}
-for i in newNums:
-    dic[i]=nums.count(i)
-print(dic)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # nums = [3,2,3]
+        newNums=[]
+        for i in nums:
+            if i not in newNums:
+                newNums.append(i)
+        dic={}
+        for i in newNums:
+            dic[i]=nums.count(i)
+        print(dic)
+        # t = (sorted(dic1.items(), key=lambda kv: (kv[1], kv[0])))
+        t=sorted(dic.items(), key=lambda k:(k[1],k[0]))
+        return (t[::-1][0][0])
