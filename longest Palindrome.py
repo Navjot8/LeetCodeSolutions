06cc4782007c6bb649b324ca29b970a4c590fc40
Navjot <navjotsinghsarwara95@gmail.com>
2021-11-07 17:52:22 -0500

    longest palindrome string

---
+++ longest Palindrome.py
@@ -0,0 +1,21 @@
+# s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+s="abcccccdd"
+dic={}
+for i in s:
+    if i not in dic:
+        dic[i]=0
+    dic[i]+=1
+count=0
+print(dic)
+flag=False
+for key,val in dic.items():
+    # print(val)
+    if val%2!=0:
+        flag=True
+    count+=val//2*2
+
+if flag:
+    print("yes")
+    count+=1
+
+print(count)
